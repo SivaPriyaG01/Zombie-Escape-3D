919228ae573bec2ece57b3b76d2794df7295da71
SivaPriyaG01 <sivapriyagarladinne@gmail.com>
2025-02-09 22:46:00 +0530

    Add enemy animations

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -1,4 +1,7 @@
+using System;
 using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -7,6 +10,7 @@ public class EnemyAI : MonoBehaviour
     private Transform player;  // Reference to the player
     private NavMeshAgent agent;
     private Animator animator;
+    private List<string> animationStates = new List<string> {"isPunching","isKicking","isBiting"};
 
     [SerializeField] private float attackRange = 2.0f;
     //[SerializeField] private float attackCooldown = 1.5f; // Delay between attacks
@@ -42,7 +46,10 @@ public class EnemyAI : MonoBehaviour
             agent.isStopped = false;
             agent.SetDestination(player.position);
             animator.SetBool("isChasing", true);
-            animator.SetBool("isAttacking", false);
+            animator.SetBool("isPunching", false);
+            animator.SetBool("isKicking", false);
+            animator.SetBool("isBiting", false);
+
         }
     }
 
@@ -51,7 +58,9 @@ public class EnemyAI : MonoBehaviour
         isAttacking = true;
         agent.isStopped = true;
         animator.SetBool("isChasing", false);
-        animator.SetBool("isAttacking", true);
+
+        int randomIndex = UnityEngine.Random.Range(0, animationStates.Count);
+        animator.SetBool(animationStates[randomIndex], true);
         transform.LookAt(player); // Face the player before attacking
 
         Debug.Log("Enemy Attacks!");
