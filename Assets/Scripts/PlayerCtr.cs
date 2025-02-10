40747c25a1d39f3482e37b687605bd62e5616ede
SivaPriyaG01 <sivapriyagarladinne@gmail.com>
2025-02-10 18:04:06 +0530

    Added tags and scripts updated

--- Assets/Scripts/PlayerCtr.cs
+++ Assets/Scripts/PlayerCtr.cs
@@ -8,13 +8,18 @@ public class PlayerCtr : MonoBehaviour
 {
     private CharacterController controller;
     private PlayerInput playerInput;
+    private EnemyAI enemyScript;
     private Animator animatorController;
     private Vector3 playerVelocity;
     private bool groundedPlayer;
+    private float dampTime = 0.02f;
     [SerializeField] float playerSpeed =10f;
     [SerializeField] float rotationSpeed =5f;
     [SerializeField] float gravityValue = -9.81f;
     [SerializeField] float jumpHeight = 2f;
+    [SerializeField] int keyCount = 0;
+    [SerializeField] int maxKeyCount = 3;
+
     
     // Start is called before the first frame update
     void Start()
@@ -22,6 +27,8 @@ public class PlayerCtr : MonoBehaviour
         controller=GetComponent<CharacterController>();
         playerInput=GetComponent<PlayerInput>();
         animatorController=GetComponent<Animator>();
+        enemyScript = FindObjectOfType<EnemyAI>();
+        
     }
 
     // Update is called once per frame
@@ -55,8 +62,8 @@ public class PlayerCtr : MonoBehaviour
             transform.Rotate(0, rotationValue*Time.deltaTime,0);
         }
 
-        animatorController.SetFloat("moveZ", inputVector.y,0.02f,Time.deltaTime);
-        animatorController.SetFloat("moveX",inputVector.x,0.02f,Time.deltaTime);
+        animatorController.SetFloat("moveZ", inputVector.y,dampTime,Time.deltaTime);
+        animatorController.SetFloat("moveX",inputVector.x,dampTime,Time.deltaTime);
     }
 
     void Jump()
@@ -69,6 +76,60 @@ public class PlayerCtr : MonoBehaviour
             animatorController.SetTrigger("Jump"); // Play jump animation if available
             Debug.Log("Jump Pressed!");
         }
+    }
+
+    // private void OnTriggerEnter(Collider other) 
+    // {
+    //     if(other.gameObject.CompareTag("SafeZone"))
+    //     {
+    //         if(keyCount==maxKeyCount)
+    //         {
+    //             Debug.Log("SafeZone entered, you won");
+    //             enemyScript.enabled=false;
+    //         }
+    //         else
+    //         {
+    //             Debug.Log("Collect all keys");
+    //         }
+    //     }
+
+    //     if(other.gameObject.CompareTag("GoldGem"))
+    //     {
+    //         keyCount++;
+    //         Debug.Log($"{keyCount} key collected");
+    //         if(keyCount==maxKeyCount)
+    //         {
+    //             Debug.Log("All keys collected, enter safe zone");
+    //         }
+    //         Destroy(other.gameObject);
+    //     }   
+    // }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit) 
+    {
+        if(hit.gameObject.CompareTag("SafeZone"))
+        {
+            if(keyCount==maxKeyCount)
+            {
+                Debug.Log("SafeZone entered, you won");
+                enemyScript.enabled=false;
+            }
+            else
+            {
+                Debug.Log("Collect all keys");
+            }
+        }
+
+        if(hit.gameObject.CompareTag("GoldGem"))
+        {
+            keyCount++;
+            Debug.Log($"{keyCount} key collected");
+            if(keyCount==maxKeyCount)
+            {
+                Debug.Log("All keys collected, enter safe zone");
+            }
+            Destroy(hit.gameObject);
+        }   
     }
 
 }
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -2,17 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemySpawnManager : MonoBehaviour
+public class SpawnManager : MonoBehaviour
 {
-    [SerializeField] GameObject EnemyToSpawn;
+    [SerializeField] GameObject enemyToSpawn;
+    [SerializeField] GameObject keysToSpawn;
+    [SerializeField] GameObject safeArea;
+
     [SerializeField] float xRange = 25f;
     [SerializeField] float zRange = 25f;
     int maxEnemiesCount = 30;
+    int maxKeyCount =3;
+    int maxSafeZone = 1;
 
     // Start is called before the first frame update
     void Start()
     {
-        SpawnEnemies();
+        SpawnObjects(maxEnemiesCount,enemyToSpawn);
+        SpawnObjects(maxKeyCount,keysToSpawn);
+        SpawnObjects(maxSafeZone,safeArea);
     }
     // Update is called once per frame
     void Update()
@@ -20,16 +27,17 @@ public class EnemySpawnManager : MonoBehaviour
         
     }
 
-    void SpawnEnemies()
+    void SpawnObjects(int count, GameObject objectToSpawn)
     {
-        for(int i =0; i< maxEnemiesCount; i++)
+        for(int i =0; i< count; i++)
         {
             float randomSpawnX = Random.Range(xRange,-xRange);
             float randomSpawnZ = Random.Range(zRange,-zRange);
         
             Vector3 randomSpawnPoint = new Vector3(randomSpawnX,0,randomSpawnZ);
-            Instantiate(EnemyToSpawn,randomSpawnPoint,Quaternion.identity);
+            Instantiate(objectToSpawn,randomSpawnPoint,Quaternion.identity);
     
         }
     }
+
 }
